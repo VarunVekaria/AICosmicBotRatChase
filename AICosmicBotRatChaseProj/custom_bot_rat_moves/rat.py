92af41f1b2593cf1dff9096b2affeab6db4f827b
Varun <varunvekaria@gmail.com>
2024-11-20 21:15:50 -0500

    comments remove

--- AICosmicBotRatChaseProj/custom_bot_rat_moves/rat.py
+++ AICosmicBotRatChaseProj/custom_bot_rat_moves/rat.py
@@ -1,5 +1,4 @@
 import random
-# from ship_environment_logic import ShipEnvironment
 
 class Rat:
     def __init__(self, environment, bot_loc):
@@ -7,12 +6,8 @@ class Rat:
         self.loc = self.environment.position_random_open_cell(avoid_cells=[bot_loc])
 
     def move(self):
-        """Move the rat to a random neighboring open cell."""
-        # Get neighboring cells
         neighbors = self.environment.get_neighbor_cells(*self.loc)
         open_neighbors = [(r, c) for r, c in neighbors if self.environment.matrix[r][c] == 0]
-
-        # Move to a random open neighbor, if any exist
         if open_neighbors:
             self.loc = random.choice(open_neighbors)
             print(f"Rat moved to {self.loc}")
